d57254b65efddf56bcdf6afbfc91150cdb2d8ef5
Ilya Dudelzak <65915836+IlyaDudelzak@users.noreply.github.com>
2024-08-10 18:33:20 +0300

    Update exercice_1.py

--- exercice_1.py
+++ exercice_1.py
@@ -1,20 +1,29 @@
 def total_salary(path):
-  with open(path, 'r') as fh:
-    total_salary = 0
-    salaries = []
-    names = []
-    lines = fh.readlines()
-    for line in lines:
-      line = line.strip()
-      l = line.split(',')
-      names.append(l[0])
-      salaries.append(int(l[1]))
-      total_salary += int(l[1])
+  try:
+    with open(path, 'r') as fh:
+      total_salary = 0
+      salaries = []
+      names = []
+      lines = fh.readlines()
+      for line in lines:
+        line = line.strip()
+        l = line.split(',')
+        names.append(l[0])
+        salaries.append(float(l[1]))
+        total_salary += float(l[1])
 
-    average_salary = int(total_salary / len(salaries))
+      average_salary = total_salary / len(salaries)
 
-    return (total_salary, average_salary)
+      return (total_salary, average_salary)
 
+  except FileNotFoundError:
+    print("Файл не найден")
+    return (0, 0)
+
+  except ValueError:
+    print("Файл поврежден")
+    return (0, 0)
 
 total, average = total_salary("salary_file.txt")
-print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
+if(total != 0 and average != 0):
+  print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
